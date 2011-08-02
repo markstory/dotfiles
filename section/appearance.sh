f9c4ee80924c500e0473f6406a99e547b164bcc0
Mark Story <mark@mark-story.com>
2011-08-02 18:15:25 -0400

    Making my prompt work on light backgrounds.  White on white is sad.

--- section/appearance.sh
+++ section/appearance.sh
@@ -3,7 +3,8 @@
 # Colours
 GREEN="\[\033[0;32m\]"
 WHITE="\[\033[0;37m\]"
-RED="\033[0;31;m\]"
+RESET="\[\033[0m\]"
+RED="\[\033[0;31;m\]"
 
 # Grep
 export GREP_OPTIONS='--color=auto'
@@ -15,13 +16,13 @@ export LSCOLORS='Gxfxcxdxdxegedabagacad'
 # Prompt config
 function exitstatus {
 	EXIT="$?"
-	PROMPT="\u:\W$GREEN$(__git_ps1)$WHITE \$ "
+	PROMPT="\u:\W$GREEN$(__git_ps1)$RESET \$ "
 
 	if [ $EXIT -eq 0 ]
 	then
 		PS1=":) $PROMPT"
 	else
-		PS1="$RED:($WHITE $PROMPT"
+		PS1="$RED:($RESET $PROMPT"
 	fi
 }
 PROMPT_COMMAND=exitstatus
