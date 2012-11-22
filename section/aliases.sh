09b2001812b50cdb4188626ada87962bb9f29d04
Mark Story <mark@freshbooks.com>
2012-11-22 15:21:36 -0500

    Add grebase command.

--- section/aliases.sh
+++ section/aliases.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
 # Aliases for various tools
 
@@ -67,6 +67,47 @@ gpush () {
 
 __git_stash_pop () {
 	[ "$1" != "No local changes to save" ] && git stash pop
+}
+
+#
+# Take a branch and do the following:
+#
+# * Stash current changes.
+# * Check out $1
+# * git pull origin $1
+# * Checkout the previous branch
+# * Rebase it on $1
+#
+grebase () {
+	local head
+	local s
+	local remote
+	s=$(git stash 2>/dev/null)
+	head=$(basename $(git symbolic-ref HEAD 2>/dev/null) 2>/dev/null)
+	remote=$2
+
+	if [ "" == "$head" ]; then
+		echo "Not on a branch, can't pull"
+		__git_stash_pop "$s"
+		return 1
+	fi
+	if [ "$1" == "" ]; then
+		echo "No branch selected, can't pull and rebase"
+		__git_stash_pop "$s"
+		return 2
+	fi
+	if [ "$remote" == "" ]; then
+		remote="origin"
+	fi
+	# Switch to the original branch and get changes
+	git checkout "$1"
+	git pull $remote "$1"
+
+	# Switch back and rebase
+	git checkout $head
+	git rebase "$1"
+	__git_stash_pop "$s"
+	return 0
 }
 
 
