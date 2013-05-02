f488fcb701f9edcf95a9c4e90b9802a155eedbd2
Mark Story <mark@mark-story.com>
2013-05-01 23:19:29 -0400

    Leave ls in the history, I often need it.

--- section/history.sh
+++ section/history.sh
@@ -9,8 +9,6 @@ export HISTCONTROL='erasedups'
 # Big history
 export HISTSIZE=5000
 
-export HISTIGNORE="ls:ls *"
-
 function rh {
 	history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head
 }
