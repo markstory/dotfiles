64f3e1da8f39c4994a1ae55a24294b6ea7f71a40
Mark Story <mark@sentry.io>
2018-12-13 16:56:58 -0800

    Look in hidden folders but not git.

--- section/fzf.sh
+++ section/fzf.sh
@@ -1,6 +1,6 @@
 # Use the_silver_searcher for fzf
 # this makes fzf faster, and exclude git guts and pyc.
-export FZF_DEFAULT_COMMAND='ag --ignore *.pyc -g ""'
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore *.pyc --ignore .git -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Add autocompletion for fzf
